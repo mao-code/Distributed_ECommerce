23748e8daf344d9af3cec7e373b59ae1bde57460
MaoXun <a22908352@gmail.com>
2025-05-24 15:22:50 +0800

    chore

--- dockerhub.bash
+++ dockerhub.bash
@@ -1,14 +1,13 @@
 # === 建 docker image 並推到 Docker Hub ===
-REG=codingmao
-docker build -t $REG/ds_ecommerce_shopping:1.0 ./shopping-service
-docker build -t $REG/ds_ecommerce_payment:1.0  ./payment-service
-docker build -t $REG/ds_ecommerce_notification:1.0 ./notification-service
-# docker build -t $REG/ds_ecommerce_front:1.0 ./frontend
-docker push $REG/ds_ecommerce_shopping:1.0 && docker push $REG/ds_ecommerce_payment:1.0 \
-  && docker push $REG/ds_ecommerce_notification:1.0 # && docker push $REG/ds_ecommerce_front:1.0
+# REG=codingmao
+# docker build -t $REG/ds_ecommerce_shopping:1.0 ./shopping-service
+# docker build -t $REG/ds_ecommerce_payment:1.0  ./payment-service
+# docker build -t $REG/ds_ecommerce_notification:1.0 ./notification-service
+# # docker build -t $REG/ds_ecommerce_front:1.0 ./frontend
+# docker push $REG/ds_ecommerce_shopping:1.0 && docker push $REG/ds_ecommerce_payment:1.0 \
+#   && docker push $REG/ds_ecommerce_notification:1.0 # && docker push $REG/ds_ecommerce_front:1.0
 
 # === 部署到 K3s ===
-kubectl apply -f k8s/kafka.yaml   # 先確定 Kafka namespace 存在
 kubectl apply -f k8s/shopping.yaml
 kubectl apply -f k8s/payment.yaml
 kubectl apply -f k8s/notification.yaml
--- payment-service/index.js
+++ payment-service/index.js
@@ -6,12 +6,12 @@ const producer = kafka.producer()
 await Promise.all([consumer.connect(), producer.connect()])
 
 await consumer.subscribe({ topic: 'orders' })
-console.log('payment listening orders…')
+console.log('payment listening orders...')
 
 await consumer.run({
   eachMessage: async ({ message }) => {
     const order = JSON.parse(message.value.toString())
-    const paid = Math.random() > 0.1          // 90% 付款成功
+    const paid = Math.random() > 0.1 // 90% 付款成功
     const msg = { ...order, paid }
     await producer.send({ topic: 'payments', messages: [{ value: JSON.stringify(msg) }] })
   }
